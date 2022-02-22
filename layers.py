bbac2d8b041e17f507c5b847adc4d711f89874f4
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-02-21 22:11:11 -0800

    Update layers.py

--- layers.py
+++ layers.py
@@ -142,9 +142,9 @@ class gatedRNNEncoder1(nn.Module):
         for weight in (self.gWeightEin, self.gWeightZwei):
             nn.init.xavier_uniform_(weight)
             
-        self.questProj = nn.Linear(hidden_size, 1)
-        self.passProj = nn.Linear(hidden_size, 1)
-        self.prevProj = nn.Linear(hidden_size, 1)
+        self.questProj = nn.Linear(hidden_size, 1, bias= False)
+        self.passProj = nn.Linear(hidden_size, 1, bias = False)
+        self.prevProj = nn.Linear(hidden_size, 1, bias = False)
 
     def forward(self, c, q, c_mask, q_mask):
         # Save original padded length for use by pad_packed_sequence
@@ -203,9 +203,8 @@ class selfAttentionRNNEncoder(nn.Module):
         for weight in (self.gWeightEin, self.gWeightZwei):
             nn.init.xavier_uniform_(weight)
             
-        self.currProj = nn.Linear(hidden_size, 1)
-        self.prevProj = nn.Linear(hidden_size, 1)
-        self.BidirectionalProj = nn.Linear(hidden_size, hidden_size)
+        self.currProj = nn.Linear(hidden_size, 1, bias = False)
+        self.prevProj = nn.Linear(hidden_size, 1, bias = False)
 
     def forward(self, v):
         # Save original padded length for use by pad_packed_sequence
@@ -236,6 +235,72 @@ class selfAttentionRNNEncoder(nn.Module):
             hJ = F.dropout(hJ, self.drop_prob, self.training)
             H.append(hJ)
         return torch.stack(H, dim = 1).squeeze(2)
+    
+    
+    
+class DAFAttention(nn.Module):
+    """Bidirectional attention originally used by BiDAF.
+
+    Bidirectional attention computes attention in two directions:
+    The context attends to the query and the query attends to the context.
+    The output of this layer is the concatenation of [context, c2q_attention,
+    context * c2q_attention, context * q2c_attention]. This concatenation allows
+    the attention vector at each timestep, along with the embeddings from
+    previous layers, to flow through the attention layer to the modeling layer.
+    The output has shape (batch_size, context_len, 8 * hidden_size).
+
+    Args:
+        hidden_size (int): Size of hidden activations.
+        drop_prob (float): Probability of zero-ing out activations.
+    """
+    def __init__(self, hidden_size, drop_prob=0.1):
+        super(DAFAttention, self).__init__()
+        self.drop_prob = drop_prob
+        self.c_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.q_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.cq_weight = nn.Parameter(torch.zeros(1, 1, hidden_size))
+        for weight in (self.c_weight, self.q_weight, self.cq_weight):
+            nn.init.xavier_uniform_(weight)
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, c, q, c_mask, q_mask):
+        batch_size, c_len, _ = c.size()
+        q_len = q.size(1)
+        s = self.get_similarity_matrix(c, q)        # (batch_size, c_len, q_len)
+        c_mask = c_mask.view(batch_size, c_len, 1)  # (batch_size, c_len, 1)
+        q_mask = q_mask.view(batch_size, 1, q_len)  # (batch_size, 1, q_len)
+        s1 = masked_softmax(s, q_mask, dim=2)       # (batch_size, c_len, q_len)
+
+        # (bs, c_len, q_len) x (bs, q_len, hid_size) => (bs, c_len, hid_size)
+        a = torch.bmm(s1, q)
+
+        #x = torch.cat([c, a, c * a, c * b], dim=2)  # (bs, c_len, 4 * hid_size)
+        x = c * a
+        return x    
+    
+    def get_similarity_matrix(self, c, q):
+        """Get the "similarity matrix" between context and query (using the
+        terminology of the BiDAF paper).
+
+        A naive implementation as described in BiDAF would concatenate the
+        three vectors then project the result with a single weight matrix. This
+        method is a more memory-efficient implementation of the same operation.
+
+        See Also:
+            Equation 1 in https://arxiv.org/abs/1611.01603
+        """
+        c_len, q_len = c.size(1), q.size(1)
+        c = F.dropout(c, self.drop_prob, self.training)  # (bs, c_len, hid_size)
+        q = F.dropout(q, self.drop_prob, self.training)  # (bs, q_len, hid_size)
+
+        # Shapes: (batch_size, c_len, q_len)
+        s0 = torch.matmul(c, self.c_weight).expand([-1, -1, q_len])
+        s1 = torch.matmul(q, self.q_weight).transpose(1, 2)\
+                                           .expand([-1, c_len, -1])
+        s2 = torch.matmul(c * self.cq_weight, q.transpose(1, 2))
+        s = s0 + s1 + s2 + self.bias
+
+        return s
 
 
 class BiDAFAttention(nn.Module):
