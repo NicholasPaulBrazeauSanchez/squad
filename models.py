617e465888da663e8e5d628d4be7b200dc630d5b
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-02-22 23:00:34 -0800

    Update models.py

--- models.py
+++ models.py
@@ -51,7 +51,7 @@ class BiDAF(nn.Module):
         self.unoPrime = layers.DAFAttention(hidden_size = 2 * hidden_size, 
                                             drop_prob = drop_prob)
         
-        self.dos = layers.selfAttentionRNNEncoder(input_size=2 * hidden_size, 
+        self.dos = layers.selfAttention(input_size=2 * hidden_size, 
                                            hidden_size=2 * hidden_size,
                                            num_layers = 1, drop_prob = drop_prob)
 
@@ -62,9 +62,9 @@ class BiDAF(nn.Module):
 
         #self.out = layers.BiDAFOutput(hidden_size=hidden_size, drop_prob=drop_prob)
         
-        #self.out2 = layers.SelfAttentionOutput(hidden_size=hidden_size, drop_prob = drop_prob)
+        self.out2 = layers.RNNOutput(hidden_size, drop_prob)
         
-        self.out3 = layers.BiDAFOutputGeneral(hidden_size= hidden_size, drop_prob = drop_prob)
+        #self.out3 = layers.BiDAFOutputGeneral(hidden_size= hidden_size, drop_prob = drop_prob)
 
     def forward(self, cw_idxs, qw_idxs):
         c_mask = torch.zeros_like(cw_idxs) != cw_idxs
@@ -84,8 +84,7 @@ class BiDAF(nn.Module):
         
         v = self.unoPrime(c_enc, q_enc, c_mask, q_mask) # (batch_size, c_len, 2 * hidden_size)
         h = self.dos(v, c_mask) # (batch_size, c_len, 2 * hidden_size)
-        #out = self.out2(h, q_enc, q_mask, c_mask) # 2 tensors, each (batch_size, c_len)
-        out = self.out3(h, q_emb, q_mask, c_mask)
+        out = self.out2(h, q_enc, q_mask, c_mask)
 
         '''
         att = self.att(c_enc, q_enc,
