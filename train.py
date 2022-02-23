c94d29c0772b839817dceb7fd586f2f733fe84aa
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-02-23 01:24:36 -0800

    Update train.py

--- train.py
+++ train.py
@@ -18,6 +18,9 @@ from args import get_train_args
 from collections import OrderedDict
 from json import dumps
 from models import BiDAF
+from models import RNNOutputSelfAttention
+from models import SelfAttention
+from models import RnnOutput
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from ujson import load as json_load
@@ -46,9 +49,26 @@ def main(args):
 
     # Get model
     log.info('Building model...')
+    '''
     model = BiDAF(word_vectors=word_vectors,
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
+    '''
+    '''
+    model = RNNOutputSelfAttention(word_vectors=word_vectors,
+                  hidden_size=args.hidden_size,
+                  drop_prob=args.drop_prob)
+    '''
+    '''
+    model = SelfAttention(word_vectors=word_vectors,
+                  hidden_size=args.hidden_size,
+                  drop_prob=args.drop_prob)
+    '''
+    #'''
+    model = RnnOutput(word_vectors=word_vectors,
+                  hidden_size=args.hidden_size,
+                  drop_prob=args.drop_prob)
+    #'''
     model = nn.DataParallel(model, args.gpu_ids)
     if args.load_path:
         log.info(f'Loading checkpoint from {args.load_path}...')
