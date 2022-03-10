14f5a38632cda27cb0a635acfe3085d6f8788060
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-03-09 19:19:31 -0800

    Update train.py

--- train.py
+++ train.py
@@ -97,20 +97,20 @@ def main(args):
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     '''
-    '''
+    #'''
     model = BiDAFCharRNNOutputCoatt(word_vectors=word_vectors,
                       char_vectors=char_vectors,
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     acceptingCharacterEmbeds = True
-    '''
     #'''
+    '''
     model = SelfAttChar(word_vectors=word_vectors,
                       char_vectors=char_vectors,
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     acceptingCharacterEmbeds = True
-    #'''
+    '''
     
     model = nn.DataParallel(model, args.gpu_ids)
     if args.load_path:
