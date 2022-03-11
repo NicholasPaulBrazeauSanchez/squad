e5decdb69cf5384573b0ac8ba144aefc6e79643a
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-03-10 20:11:35 -0800

    Update test.py

--- test.py
+++ test.py
@@ -23,6 +23,7 @@ from collections import OrderedDict
 from json import dumps
 from models import BiDAF
 from models import RnnOutput
+from models import BiDAFCharRNNOutput
 from os.path import join
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
@@ -41,13 +42,19 @@ def main(args):
     # Get embeddings
     log.info('Loading embeddings...')
     word_vectors = util.torch_from_json(args.word_emb_file)
+    char_vectors = util.torch_from_json(args.char_emb_file)
 
     # Get model
     log.info('Building model...')
     #model = BiDAF(word_vectors=word_vectors,
     #              hidden_size=args.hidden_size)
-    model = RnnOutput(word_vectors=word_vectors,
-                      hidden_size=args.hidden_size)
+    #model = RnnOutput(word_vectors=word_vectors,
+    #                  hidden_size=args.hidden_size)
+    model = BiDAFCharRNNOutput(word_vectors=word_vectors,
+                      char_vectors=char_vectors,
+                  hidden_size=args.hidden_size,
+                  drop_prob=args.drop_prob)
+    acceptingCharacterEmbeds = True
     model = nn.DataParallel(model, gpu_ids)
     log.info(f'Loading checkpoint from {args.load_path}...')
     model = util.load_model(model, args.load_path, gpu_ids, return_step=False)
@@ -81,7 +88,7 @@ def main(args):
             batch_size = cw_idxs.size(0)
 
             # Forward
-            log_p1, log_p2 = model(cw_idxs, qw_idxs)
+            log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
             y1, y2 = y1.to(device), y2.to(device)
             loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
             nll_meter.update(loss.item(), batch_size)
