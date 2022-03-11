bde392850b234d22c0db8ba9f598eff1b182afcb
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-03-11 04:07:48 -0800

    Update models.py

--- models.py
+++ models.py
@@ -84,8 +84,8 @@ class RNET(nn.Module):
         self.att = layers.BiDAFAttention(hidden_size=2 * hidden_size,
                                         drop_prob=drop_prob)
         
-        self.att = layers.DAFAttention(hidden_size=2 * hidden_size,
-                                         drop_prob=drop_prob)
+        #self.att = layers.DAFAttention(hidden_size=2 * hidden_size,
+         #                                drop_prob=drop_prob)
         '''
         self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
                                      hidden_size=hidden_size,
