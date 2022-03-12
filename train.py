8728e7a1229b49f879f04b0be770f25ba9a19734
NicholasPaulBrazeauSanchez <55761068+NicholasPaulBrazeauSanchez@users.noreply.github.com>
2022-03-12 11:12:46 -0800

    Update train.py

--- train.py
+++ train.py
@@ -62,13 +62,13 @@ def main(args):
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     '''
-    #'''
+    '''
     model = BiDAFChar(word_vectors=word_vectors,
                       char_vectors=char_vectors,
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     acceptingCharacterEmbeds = True
-    #'''
+    '''
     '''
     model = RNNOutputSelfAttention(word_vectors=word_vectors,
                   hidden_size=args.hidden_size,
@@ -84,13 +84,13 @@ def main(args):
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     '''
-    '''
+    #'''
     model = BiDAFCharRNNOutput(word_vectors=word_vectors,
                       char_vectors=char_vectors,
                   hidden_size=args.hidden_size,
                   drop_prob=args.drop_prob)
     acceptingCharacterEmbeds = True
-    '''
+    #'''
     '''
     model = RNET(word_vectors=word_vectors,
                  char_vectors=char_vectors,
